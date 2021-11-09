9430f96ba13843847b17d202ec162d0dd2200d9f
ElvisRequejo <73666412+ElvisRequejo@users.noreply.github.com>
2021-11-09 17:03:11 -0500

    Delete src directory

--- src/main/java/com/example/examen/repository/EditorialRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.examen.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.examen.entity.Editorial;
-
-public interface EditorialRepository extends JpaRepository<Editorial, Integer> {
-
-}
